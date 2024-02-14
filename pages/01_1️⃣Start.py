379135503c152fc58a7d952a4854c446e73a3859
kailiangs_gpu <1972475358@qq.com>
2024-02-14 17:24:37 +0800

    初始化app架构

---
+++ pages/01_1️⃣Start.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title='Start with your dataset', 
+                   page_icon=':one:')
+
+
+st.title(body=":one: Start with your dataset")
+
+st.write('#')
